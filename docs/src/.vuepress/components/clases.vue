271fa2dce4edfdcc93fa3f715d71948d4b1e7776
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-04 13:49:15 +0000

    clases component with week counter

--- docs/src/.vuepress/components/clases.vue
+++ docs/src/.vuepress/components/clases.vue
@@ -1,16 +1,37 @@
 <template>
+    <div class="clases">
+    <h2>Llevamos {{ numberOfClasses}} clases y {{ numberOfWeeks }} semanas</h2>
     <ol reversed> 
-        <li v-for="page in classFiles" :key="page.key"> <a :href="page.path">{{ page.title }}</a> 
-        <ul>
-          <li>{{ page.frontmatter.summary }}</li>
-          <li v-if="page.frontmatter.video"><a :href="getUrl(page.frontmatter.video)" target="_blank">Vídeo</a></li>
-        </ul>
+        <li v-for="(page, index) in classFiles" :key="page.key"> <a :href="page.path">{{ page.title }}</a>  
+        <b v-if="(index < classFiles.length - 2) && (week(page) !== week(classFiles[index+1]))">{{ week(page) }}ª  semana</b>
+        <b v-else>{{ week(page) }}ª  semana</b>
+            <ul>
+                <li>{{ page.frontmatter.summary }}</li>
+                <li v-if="page.frontmatter.video"><a :href="getUrl(page.frontmatter.video)" target="_blank">Vídeo</a></li>
+            </ul>
         </li>
     </ol>
+    </div>
 </template>
 
 <script>
     import * as path from 'path';
+
+    function weekOfTheYear(date) {
+        var d = new Date(+date);
+        d.setHours(0, 0, 0);
+        d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+        return Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7);
+    }
+    
+    function getWeeksDiff(startDate, endDate) {
+      const msInWeek = 1000 * 60 * 60 * 24 * 7;
+
+      return Math.round(Math.abs(endDate - startDate) / msInWeek);
+    }
+    
+    const firstLessonDate = new Date("2022-10-03");
+    const firstWeek = weekOfTheYear(firstLessonDate);
 
     export default { 
         data() {
@@ -30,6 +51,18 @@
             getUrl(idOrurl) {
                 if (/https/.test(idOrurl)) return idOrurl;
                 return "https://youtu.be/"+idOrurl;
+            },
+            week(page) {
+                //console.log(firstWeek);
+                let [year, month, day] = this.getDate(page).split(/[-]/);
+                console.log(year, month, day);
+                
+                let date = new Date(`${month}/${day}/${year}`);
+                console.log(date);
+                const weekDiff = getWeeksDiff(firstLessonDate, date);
+                console.log(weekDiff);
+                return weekDiff+1;
+                
             }
         },
         computed: {
@@ -50,11 +83,18 @@
                 let clases = this.$site.pages.filter(page => /clases.\d+/.test(page.relativePath));
                 //console.log(this.getDate(clases[0]));
                 return clases.sort( compare );
-            }, 
+            },
             video() {
                 url = this.page.frontmatter.video
                 return url;
+            },
+            numberOfClasses() {
+                return this.classFiles.length;
+            },
+            numberOfWeeks() {
+                return this.week(this.classFiles[0]) - this.week(this.classFiles[this.classFiles.length - 1]) + 1;
             }
+
         }
     }
 </script>
