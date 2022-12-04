dab2e8c25ffed35bb62667869cbc789b5000f0c2
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-12-04 11:32:04 +0000

    2223

--- docs/src/.vuepress/temas-publicados.js
+++ docs/src/.vuepress/temas-publicados.js
@@ -1,45 +1,52 @@
 module.exports = [
     {
-        text: 'Introduction to SYTWS', 
+        text: 'Introduction to SYTWS',
         items: [
-            {text: 'Apartados', link: '/temas/'},
-            {text: "Intro a SYTWS", link:'/temas/introduccion-a-sytws/guia-docente.html'},
+            { text: 'Apartados', link: '/temas/' },
+            { text: "Intro a SYTWS", link: '/temas/introduccion-a-sytws/guia-docente.html' },
         ]
     },
     {
-        text: 'Fundamentos', 
+        text: 'Fundamentos',
         items: [
-            { text: "Apartados", link:'/temas/introduccion-a-javascript/'},
-            { text: 'GitHub Cli', link:'/temas/introduccion-a-javascript/github-cli/'},
+            { text: "Apartados", link: '/temas/introduccion-a-javascript/' },
+            { text: 'GitHub Cli', link: '/temas/introduccion-a-javascript/github-cli/' },
         ]
     },
     {
-        text: 'Asynchronous JS', 
+        text: 'Asynchronous JS',
         items: [
-            { text: 'Apartados', link:'/temas/async/'},
-            { text: 'The Event Loop', link:'/temas/async/event-lopp/'},
-            { text: 'Event Emitters', link:'/temas/async/event-emitter'},
-            { text: 'Web Workers', link:'/temas/async/web-workers'},
+            { text: 'Apartados', link: '/temas/async/' },
+            { text: 'The Event Loop', link: '/temas/async/event-lopp/' },
+            { text: 'Event Emitters', link: '/temas/async/event-emitter' },
+            { text: 'Web Workers', link: '/temas/async/web-workers' },
         ]
     },
     {
-        text: 'Servicios y Aplicaciones Web', 
-        items: [ 
-            { text: 'Jekyll', link: '/temas/web/jekyll.html'},
+        text: 'Servicios y Aplicaciones Web',
+        items: [
+            { text: 'Jekyll', link: '/temas/web/jekyll.html' },
             {
-              text: 'GitHub GraphQL API Examples', 
-              link: '/temas/introduccion-a-javascript/github-cli/gh-api-graphql'
+                text: 'GitHub REST API Examples',
+                link: '/temas/introduccion-a-javascript/github-cli/gh-graphql'
+            },
+            {
+                text: 'GitHub GraphQL API Examples',
+                link: '/temas/introduccion-a-javascript/github-cli/gh-api-graphql'
             },
             {
                 text: 'jq Introduction',
                 link: '/temas/web/jq-introduction'
             },
-            { 
-              text: 'OAuth Authentication with GitHub as Oauth2 Provider and Netlify providing the Server', 
-              link: '/temas/web/netlify-github-oauth.html'},
-            { text: 'Introduction to Serverless',
-              link: '/temas/web/serverless.html'},
+            {
+                text: 'OAuth Authentication with GitHub as Oauth2 Provider and Netlify providing the Server',
+                link: '/temas/web/netlify-github-oauth.html'
+            },
+            {
+                text: 'Introduction to Serverless',
+                link: '/temas/web/serverless.html'
+            },
         ]
-    }, 
+    },
 
 ];
