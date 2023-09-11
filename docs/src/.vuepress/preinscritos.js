c1a3e5c3b11275507435c3a81980cc8047ded1f6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-09-11 15:48:58 +0100

    2324

---
+++ docs/src/.vuepress/preinscritos.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "2324": [
+        {
+            nombre: "EUGENIO JOSE GONZALEZ LUIS",
+            "email": "alu0101000756@ull.edu.es"
+        },
+        {
+            nombre: "MARCOS JESÚS BARRIOS LORENZO",
+            email: "alu0101056944@ull.edu.es"
+        },
+        {
+            nombre: "Elena Montserrat Sacramento Delgado",
+            email: "alu0100884524@ull.edu.es"
+        },
+        {
+            nombre: "Miriam Nuñez García",
+            email: "alu0100945621@ull.edu.es"
+        },
+        {
+            nombre: "YAGO PÉREZ MOLANES",
+            email: "alu0101254678@ull.edu.es"
+        }
+    ]
+}
+
+
