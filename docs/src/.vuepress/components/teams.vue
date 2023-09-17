1483132f836dad3d753739f9506a2724689aec63
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-09-17 11:48:19 +0100

    2324

--- docs/src/.vuepress/components/teams.vue
+++ docs/src/.vuepress/components/teams.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>{{ totalCount }} Teams</h2>
     <ul>
-      <li v-for="team in teams" :key="team.name"> <a :href="team.url">{{ team.name }}</a>
+      <li v-for="team in teams" :key="team.name"> <a :href="team.url">{{ team.name }} ({{ team.length }} member)</a>
         <ul>
           <li><a :href="team.repositories" target="_blank">Repositories</a></li>
           <li><a :href="team.userUrl" target="_blank">{{ team.userName }}</a></li>
@@ -30,9 +30,30 @@ export default {
   },
   computed: {
     teams() {
+      if (!this.rawTeams || !Array.isArray(this.rawTeams)) {
+        return []
+      }
       return this.rawTeams.map(team => {
-        let node = team.node
-        let member = node.members.edges[0].node
+        let node = team.node;
+        //console.log(node.name)
+        //console.log(node.url)
+        let members = node?.members?.edges
+        if (!members || !Array.isArray(members) || members.length === 0) {
+          //console.log(members);
+          //console.log('no members')
+          return {
+            name: node.name,
+            url: node.url,
+            repositories: node.url + '/repositories',
+            userName: capitalizeFirstLetter(node.name.split(/[-_]/)[0]),
+            userUrl: '',
+            notifications: '',
+            length: 0,
+          }
+        }
+        let member = members[0].node
+        //console.log(member.login)
+        //console.log(member.name)
         const user = {
           name: node.name,
           url: node.url,
@@ -40,6 +61,7 @@ export default {
           userName: member.name || capitalizeFirstLetter(node.name.split(/[-_]/)[0]),
           userUrl: member.url,
           notifications: `https://github.com/notifications?query=author%3A${member.login}`,
+          length: members.length,
         }
         return user
       })
