0b82f2471b1b60e485f31eac3d6ce1c2e3869d18
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-09-17 03:19:00 +0100

    2425

--- docs/src/.vuepress/teams.mjs
+++ docs/src/.vuepress/teams.mjs
@@ -1,1 +1,16 @@
-export default { "data": { "organization": { "teams": { "totalCount": 1, "edges": [ { "node": { "name": "casiano-rodriguez-leon-alu0100291865", "members": { "totalCount": 1, "edges": [ { "memberAccessUrl": "https://github.com/orgs/ULL-MII-SYTWS-2425/people/casiano-rodriguez", "node": { "name": "Casiano as ULL alumni", "url": "https://github.com/casiano-rodriguez", "email": "", "login": "casiano-rodriguez" } } ] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/casiano-rodriguez-leon-alu0100291865" } } ] } } } }
+export default {
+  "data": {
+    "organization": {
+      "teams": {
+        "totalCount": 6, "edges": [{
+          "node":
+            { "name": "casiano-rodriguez-leon-alu0100291865", "members": { "totalCount": 1, "edges": [{ "memberAccessUrl": "https://github.com/orgs/ULL-MII-SYTWS-2425/people/casiano-rodriguez", "node": { "name": "Casiano as ULL alumni", "url": "https://github.com/casiano-rodriguez", "email": "", "login": "casiano-rodriguez" } }] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/casiano-rodriguez-leon-alu0100291865" }
+        },
+        { "node": { "name": "daniel_felipe-gomez-alu0101438139", "members": { "totalCount": 1, "edges": [{ "memberAccessUrl": "https://github.com/orgs/ULL-MII-SYTWS-2425/people/DanielFelipeGomez", "node": { "name": "Daniel Felipe Gómez Aristizabal", "url": "https://github.com/DanielFelipeGomez", "email": "", "login": "DanielFelipeGomez" } }] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/daniel_felipe-gomez-alu0101438139" } }, 
+        { "node": { "name": "ismael-martin-herrera-alu0101397375", "members": { "totalCount": 1, "edges": [{ "memberAccessUrl": "https://github.com/orgs/ULL-MII-SYTWS-2425/people/imh-02", "node": { "name": "Ismael Martín Herrera", "url": "https://github.com/imh-02", "email": "ismael.martin.26@ull.edu.es", "login": "imh-02" } }] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/ismael-martin-herrera-alu0101397375" } },
+        { "node": { "name": "nicolas-aranda-robayna-alu0123456789", "members": { "totalCount": 1, "edges": [{ "memberAccessUrl": "https://github.com/orgs/ULL-MII-SYTWS-2425/people/nicarandar", "node": { "name": "Nicolás Aranda", "url": "https://github.com/nicarandar", "email": "", "login": "nicarandar" } }] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/nicolas-aranda-robayna-alu0123456789" } }, 
+        { "node": { "name": "rebeca-rodriguez-alu0101394763", "members": { "totalCount": 1, "edges": [{ "memberAccessUrl": "https://github.com/orgs/ULL-MII-SYTWS-2425/people/rrrguez", "node": { "name": "Rebeca Rguez", "url": "https://github.com/rrrguez", "email": "", "login": "rrrguez" } }] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/rebeca-rodriguez-alu0101394763" } }, { "node": { "name": "rebeca-rodriguez-rodriguez-alu0101394763", "members": { "totalCount": 0, "edges": [] }, "url": "https://github.com/orgs/ULL-MII-SYTWS-2425/teams/rebeca-rodriguez-rodriguez-alu0101394763" } }]
+      }
+    }
+  }
+}
